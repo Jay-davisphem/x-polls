b21aa98b4a23100582e674d982f0ccb983183f71
David Oluwafemi Joshua <davidoluwafemi178@gmail.com>
2022-03-09 01:28:23 +0100

    change admin

--- polls/admin.py
+++ polls/admin.py
@@ -8,8 +8,7 @@ class ChoiceInline(admin.TabularInline):
 
 
 class QuestionAdmin(admin.ModelAdmin):
-    fieldsets = [(None, {'fields': ['question_text']}), ('Date information', {
-        'fields': ['pub_date'], 'classes': ['collapse']}), ]
+    fieldsets = [(None, {'fields': ['question_text']}), ]
     inlines = [ChoiceInline]
     list_display = ('question_text', 'pub_date', 'was_published_recently')
     list_filter = ['pub_date']
